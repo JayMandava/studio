c017604afc6894c6980de7e3223dfff1ceafdf90
JayMandava <jeyanth.mandava@insurgence.ai>
2025-11-29 12:38:23 +0530

    feat: UI/UX changes

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -13,15 +13,15 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      "peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
+      "peer grid place-items-center h-5 w-5 shrink-0 rounded-md border-2 border-primary/70 bg-background text-primary ring-offset-background transition focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
       className
     )}
     {...props}
   >
     <CheckboxPrimitive.Indicator
-      className={cn("flex items-center justify-center text-current")}
+      className={cn("flex items-center justify-center text-primary-foreground")}
     >
-    <CheckIcon fontSize="small" className="h-4 w-4" />
+      <span className="block h-2.5 w-2.5 rounded-sm bg-current" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ))
