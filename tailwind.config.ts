e27d24066af01ffe927fe9500334c68b8eb0cd62
JayMandava <jeyanth.mandava@insurgence.ai>
2025-11-29 10:17:46 +0530

    feat: Refined the UI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        body: ['Roboto', 'system-ui', 'sans-serif'],
+        headline: ['Roboto', 'system-ui', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
