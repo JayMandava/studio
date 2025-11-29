2ba217bc2d11050adca11b7f73a92ade20c4430a
JayMandava <jeyanth.mandava@insurgence.ai>
2025-11-29 10:48:23 +0530

    feat: Reverted UI changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Roboto', 'system-ui', 'sans-serif'],
-        headline: ['Roboto', 'system-ui', 'sans-serif'],
+        body: ['Inter', 'sans-serif'],
+        headline: ['Inter', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
